c618febe9b932de000a3b8e132dff32762777e72
victor13bars <victor_bars_bars95@mail.ru>
2022-08-03 15:23:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const TelegramApi = require('node-telegram-bot-api')
+const {gameOptions, againOptions} = require('./options')
+const token = '5525114523:AAE5J80i5dZ6cl4xg74dFqeDQ4qWL2jwNZk'
+
+const bot = new TelegramApi(token, {polling: true})
+
+const chats = {}
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, `Сейчас я загадаю число от 0 до 9, а ты должен её угадать`)
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, 'Отгадывай!', gameOptions)
+}
+
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Начальное приветствие'},
+        {command: '/info', description: 'Получить информацию о пользователе'},
+        {command: '/game', description: 'Игра угадай цифру'}
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text
+        const chatId = msg.chat.id
+
+        if (text === '/start') {
+            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/eb7/80f/eb780f5f-99e7-4005-8c35-898b61b096cd/14.webp')
+            return bot.sendMessage(chatId, `Добро пожаловать в мой телеграм бот`)
+        }
+
+        if (text === '/info') {
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.chat.first_name} ${msg.chat.last_name}`)
+        }
+        if (text === '/game') {
+            return startGame(chatId)
+        }
+        return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй ещё раз`)
+    })
+
+    bot.on('callback_query', msg => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+        if (data === chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю, ты угадал цифру ${chats[chatId]}`, againOptions)
+        } else {
+            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот выбрал цифру ${chats[chatId]}`, againOptions)
+        }
+    })
+}
+
+start()
